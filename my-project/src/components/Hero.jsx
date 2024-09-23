7c4db422d9efd1e218c4143f4bb11a327048ae9c
e.an <161005688+estateangela@users.noreply.github.com>
2024-09-23 18:17:13 +0800

    import react use vite and build page

---
+++ my-project/src/components/Hero.jsx
@@ -0,0 +1,10 @@
+const Hero = () => {
+  return (
+    <section className="w-full nav-height bg-black relative">
+        <div className="h-5/6"></div>
+      
+    </section>
+  )
+}
+ 
+export default Hero
---
+++ my-project/src/components/Highlights.jsx
@@ -0,0 +1,9 @@
+const Highlights = () => {
+  return (
+    <div>
+      {/* 组件内容 */}
+    </div>
+  );
+};
+
+export default Highlights; // 确保有这个默认导出
