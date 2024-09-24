1ec383df51d0c513d45edb2fc9e54b3840b9ef5c
e.an <161005688+estateangela@users.noreply.github.com>
2024-09-24 21:59:12 +0800

    update

--- my-project/src/components/Highlights.jsx
+++ my-project/src/components/Highlights.jsx
@@ -1,11 +1,12 @@
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { rightImg, watchImg } from "../utils";
+import VideoCarousel from "./VideoCarousel";
 
 const Highlights = () => {
   useGSAP(() => {
     gsap.to('#title' , { opacity:1 , y:0 })
-    gsap.to('.link' , { opacity:1 , y:0 })
+    gsap.to('.link' , { opacity:1 , y:0 , duration:1, stagger:0.25})
 
   },[])
   return (
@@ -24,6 +25,9 @@ const Highlights = () => {
             <img src={rightImg} alt="right" className="ml-2"/>
           </p>
         </div>
+      </div>
+      <div>
+        <VideoCarousel/>
       </div>
      
     </section>
---
+++ my-project/src/components/VideoCarousel.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { hightlightsSlides } from '../constans'
+
+const VideoCarousel = () => {
+  return (
+    <>
+    <div className='flex items-center'>
+        {hightlightsSlides.map((list,i) => (
+            <div key={list.id} id='slider' className='sm:pr-20 pr-10'>
+                <div className='video-carousel_container'>
+                    <div className='w-full h-full flex-center
+                    rounded-3xl overflow-hidden bg-black'>
+                        <video>
+                            <source src={list.video} type='video/mp4'/>
+                        </video>
+                    </div>
+                    </div>
+                    </div>
+        ))}
+      
+    </div>
+    </>
+  )
+}
+
+export default VideoCarousel
