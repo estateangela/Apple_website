2511fc0346cbd66704a2696e56d5bd1b7ba9d3ec
e.an <161005688+estateangela@users.noreply.github.com>
2024-09-26 15:06:14 +0800

    fix and update
    fix the model problem and change the web name

--- my-project/src/components/ModelView.jsx
+++ my-project/src/components/ModelView.jsx
@@ -1,18 +1,23 @@
-import { PerspectiveCamera, View, OrbitControls, Html } from "@react-three/drei"
-import Lights from "./Light"
-import { Suspense } from "react"
-import IPhone from "./IPhone"
-import * as THREE from 'three'
+import { Html,OrbitControls,PerspectiveCamera,View } from "@react-three/drei";
+
+import * as THREE from 'three';
+import Loader from "./Loader";
+import IPhone from "./IPhone";
+import Lights from "./Light";
+import { Suspense } from "react";
 
 const ModelView = ({index,groupRef,gsapType,
-    controlRef,setRotationSize,size,item}) => {
+    controlRef,setRotationState,size,item}) => {
   return (
     <View index={index}
     id={gsapType}
-    className={`w-full h-full ${index === 2 ? 'right-[-100%]' : ''}`}>
+    className={`w-full h-full absolute ${index === 2 ? 'right-[-100%]' : ''}`}>
         <ambientLight intensity={0.3}/>
+
         <PerspectiveCamera makeDefault position={[0,0,4]}/>
+
         <Lights/>
+
         <OrbitControls
          makeDefault
          ref={controlRef}
@@ -20,17 +25,17 @@ const ModelView = ({index,groupRef,gsapType,
          enablePan={false}
          rotateSpeed={0.4}
          target={new THREE.Vector3(0,0,0)}
-         onEnd={() => setRotationState(controlRef.current.getAzimuthalAngel())}
+         onEnd={() => setRotationState(controlRef.current.getAzimuthalAngle())}
 
         />
-        <group ref={groupRef} name={`{index === 1} ? 'small' : 'large'`} 
+        <group ref={groupRef} name={`${index === 1} ? 'small' : 'large'`} 
         position={[0,0,0]}>
-
-       
-        <Suspense fallback={<Html><div>Loading</div></Html>}>
+        <Suspense fallback={<Loader/>}>
         
         <IPhone 
-        scale={index === 1 ? [15,15,15] : [17,17,17]} />
+        scale={index === 1 ? [15,15,15] : [17,17,17]}
+        item={item}
+        size={size} />
         </Suspense>
         </group>
     </View>
